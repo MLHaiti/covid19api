57f093d5848875f3efd7d51a3f2d3f0901d3a29f
thecassion <pierrerobentz.cassion@gmail.com>
2020-03-29 18:27:55 -0400

    Anyone can create a person

--- app/main/controller/people_controller.py
+++ app/main/controller/people_controller.py
@@ -4,19 +4,20 @@ from flask_restx import Resource
 from app.main.util.decorator import admin_token_required
 from ..util.dto import PeopleDto
 from ..service.user_service import save_new_user, get_all_users, get_a_user
+from ..service.person_service import get_all_people,get_a_person, save_new_person
 
 api = PeopleDto.api
 _person = PeopleDto.people
 
 
 @api.route('/')
-class UserList(Resource):
+class PeopleList(Resource):
     @api.doc('list_of_people')
-    @admin_token_required
+    # @admin_token_required
     @api.marshal_list_with(_person, envelope='data')
     def get(self):
         """List all People"""
-        return get_all_users()
+        return get_all_people()
 
     @api.expect(_person, validate=True)
     @api.response(201, 'Person successfully created.')
@@ -24,19 +25,19 @@ class UserList(Resource):
     def post(self):
         """Creates a new Person """
         data = request.json
-        return save_new_user(data=data)
+        return save_new_person(data=data)
 
 
 @api.route('/<id>')
 @api.param('id', 'The Person identifier')
 @api.response(404, 'Person not found.')
-class User(Resource):
+class People(Resource):
     @api.doc('get a person')
     @api.marshal_with(_person)
     def get(self, public_id):
         """get a user given its identifier"""
-        user = get_a_user(public_id)
-        if not user:
+        person = get_a_person(id)
+        if not person:
             api.abort(404)
         else:
-            return user
+            return person
